aca6678ef2edf6bff3685964649f3844ae6d7441
Catherine Wang <cawa@microsoft.com>
2024-10-26 18:38:25 -0700

    add PreloadRedis proejct

---
+++ PreloadRedis/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PreloadRedis;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddHttpClient<ProductHTTPClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ProductApiUrl"]);
+});
+
+using IHost host = builder.Build();
+
+ProductHTTPClient productHTTPClient = host.Services.GetRequiredService<ProductHTTPClient>();
+await productHTTPClient.ReadProduct();
+
+
