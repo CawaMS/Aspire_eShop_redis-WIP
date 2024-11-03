3fb95d0fc2e6f29cd5a844ffa3f8bf02b279da18
Catherine Wang <cawa@microsoft.com>
2024-11-02 23:55:22 -0700

    customized azd bicep scripts to create AMR Balanced B5 with HA. Working end-to-end in Azure. Using Connection Strings

--- Products/Program.cs
+++ Products/Program.cs
@@ -1,7 +1,4 @@
-using Aspire.StackExchange.Redis;
-using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 using Products.Data;
 using Products.Endpoints;
 using StackExchange.Redis;
@@ -9,17 +6,15 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+//builder.Services.AddDbContext<ProductDataContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("ProductsContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
 
+// builder.AddSqlServerDbContext<ProductDataContext>("ProductsContext");
 builder.Services.AddDbContext<ProductDataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProductContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
 
-// Add Redis cache, with Entra ID
-var _redisHostName = builder.Configuration.GetConnectionString("cache");
-var configurationOptions = await ConfigurationOptions.Parse($"{_redisHostName}").ConfigureForAzureWithTokenCredentialAsync(new DefaultAzureCredential());
-
-Console.WriteLine($"Redis connection string: {_redisHostName}");
-
-builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(configurationOptions));
+// Add Redis cache
+builder.AddRedisClient("cache");
 
 // Add services to the container.
 var app = builder.Build();
---
+++ eShopLite.AppHost/infra/cache/cache.module.bicep
@@ -0,0 +1,53 @@
+targetScope = 'resourceGroup'
+
+@description('')
+param location string = resourceGroup().location
+
+@description('')
+param keyVaultName string
+
+
+resource keyVault_IeF8jZvXV 'Microsoft.KeyVault/vaults@2022-07-01' existing = {
+  name: keyVaultName
+}
+
+resource redisCache_enclX3umP 'Microsoft.Cache/redisEnterprise@2024-05-01-preview' = {
+  name: toLower(take('cache${uniqueString(resourceGroup().id)}', 24))
+  location: location
+  tags: {
+    'aspire-resource-name': 'cache'
+  }
+  sku: {
+    name: 'Balanced_B5'
+  }
+  identity: {
+    type: 'None'
+  }
+  properties: {
+    minimumTlsVersion: '1.2'
+  }
+}
+
+resource redisEnterpriseDatabase 'Microsoft.Cache/redisEnterprise/databases@2024-05-01-preview' = {
+  name: 'default'
+  parent: redisCache_enclX3umP
+  properties:{
+    clientProtocol: 'Encrypted'
+    port: 10000
+    clusteringPolicy: 'OSSCluster'
+    evictionPolicy: 'NoEviction'
+    persistence:{
+      aofEnabled: false 
+      rdbEnabled: false
+    }
+  }
+}
+
+resource keyVaultSecret_Ddsc3HjrA 'Microsoft.KeyVault/vaults/secrets@2022-07-01' = {
+  parent: keyVault_IeF8jZvXV
+  name: 'connectionString'
+  location: location
+  properties: {
+    value: '${redisCache_enclX3umP.properties.hostName}:10000,ssl=true,password=${redisEnterpriseDatabase.listKeys().primaryKey},abortConnect=False'
+  }
+}
