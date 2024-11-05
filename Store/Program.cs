f3a16f67dcbde81e3dc117360dbe64a84c4acbe1
Catherine Wang <cawa@microsoft.com>
2024-11-04 17:30:01 -0800

    add thumb up button

--- Store/Program.cs
+++ Store/Program.cs
@@ -81,6 +81,8 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddSingleton<IEmailSender<StoreUser>, IdentityNoOpEmailSender>();
 
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
